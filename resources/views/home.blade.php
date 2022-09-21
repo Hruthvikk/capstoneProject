50a1841a88734a54ede8f6fb4b93105545b03d6f
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-09-21 18:29:58 -0400

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,35 @@
         <div class="pr">
             <div class="prr images">
                 <img src="/images/Food1.jpg" alt="" height="200px" width="200px">
+                <p>Roti Sabji</p>
             </div>
             <div class="prr images">
-                <img src="/images/Food2.jpg" alt=""  height="150px" width="150px">
+                <img src="/images/Food4.jpg" alt=""  height="150px" width="150px">
+                <p>Chai</p>
             </div>
             <div class="prr images">
-                <img src="/images/Food3.jpg" alt=""  height="150px" width="150px">
+                <img src="/images/Food6.jpg" alt=""  height="150px" width="150px">
+                <p>Samosa</p>               
             </div>
         </div>
     </div>
+    <br>
     <div class="row">
     <h2 style="text-align: center;">Meals</h2>
-
+    <div class="pr">
+            <div class="prr images">
+                <img src="/images/Food1.jpg" alt="" height="200px" width="200px">
+                <p>Breakfast</p>
+            </div>
+            <div class="prr images">
+                <img src="/images/Food4.jpg" alt=""  height="150px" width="150px">
+                <p>Lunch</p>
+            </div>
+            <div class="prr images">
+                <img src="/images/Food6.jpg" alt=""  height="150px" width="150px">
+                <p>Dinner</p>               
+            </div>
+        </div>
     </div>
 </div>
 @endsection
