ead92c68c7d2b3b944cb26cb1152248beab8e7b9
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-09-21 15:57:19 -0400

    first Deploy

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('main')
+@section('content')
+<div class="container">
+    <div class="row">
+        <h2 style="text-align: center;">Popular Recipe</h2>
+        <div class="pr">
+            <div class="prr images">
+                <img src="images/food1.jpg" alt="" height="200px" width="200px">
+            </div>
+            <div class="prr images">
+                <img src="/images/Food2.jpg" alt=""  height="150px" width="150px">
+            </div>
+            <div class="prr images">
+                <img src="/images/Food3.jpg" alt=""  height="150px" width="150px">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+    <h2 style="text-align: center;">Meals</h2>
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/main.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Neon Approns</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <link rel="stylesheet" href="app.css">
+        
+    </head>
+    <body>
+    <nav nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('/')}}">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('/aboutus')}}">About Us</a>    
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('/signInUp')}}">Sign in / Sign up</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+
+        @yield('content')
+        <br>
+        <footer class="stickFoot">
+            <div>
+                <div class="col">©</div>
+                <div class="col">Hruthvik</div>
+                <div class="col">Chokshi</div>
+            </div>
+        </footer>
+    </body>
+</html>
