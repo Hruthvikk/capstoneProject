c54246eaa391b7ddb73309f60c1e7cf16e8da02c
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-06 14:22:15 -0500

    check2

--- resources/views/check.blade.php
+++ resources/views/check.blade.php
@@ -2,7 +2,8 @@
 @section('content')
 
     @foreach ($recip as $r)
-        <span>{{$r->recipeImage}}</span>        
+        <span>{{$r->recipeImage}}</span>
+        <img src="{{asset('storage/images/'. $r->recipeImage)}}">
     @endforeach
 
 @endsection
