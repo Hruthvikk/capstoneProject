4ecdc5472e2cfbfacdfbfffbf57bc4cad719f599
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-17 21:09:40 -0500

    rating recipe unique10

--- app/Http/Controllers/ratingFavourite1.php
+++ app/Http/Controllers/ratingFavourite1.php
@@ -11,23 +11,24 @@ class ratingFavourite1 extends Controller
     public function raterecipe(Request $request){
         $current_uid = $request->session()->get('loginUserId') ;
         $inputsn = $request->input('rate');
-        $query = "SELECT starNum FROM rating_favs where user_id=$current_uid";
+        $query = "SELECT * FROM rating_favs where user_id=$current_uid";
         $csn[] = DB::select($query);
+        print_r($csn);
         $new_raterecipe = new ratingFav();
-        if( ( $csn = $inputsn )&& ($new_raterecipe->user_id = $request->user_id)){
-            return back()->with('alreadyexists','Same rating already Exists');    
-        }
-        else{
+        // if( ( $csn = $inputsn )&& ($new_raterecipe->user_id = $request->user_id)){
+        //     return back()->with('alreadyexists','Same rating already Exists');    
+        // }
+        // else{
 
-                $new_raterecipe->starNum = $request->input('rate');
-                $new_raterecipe->favYesNo = $request->input('fav');
-                $new_raterecipe->user_id = $request->user_id;
-                $new_raterecipe->recipe_id = $request->recipe_id;
-                $res = $new_raterecipe->save();
-                if($res){
-                return back();
-                }
-        }
+        //         $new_raterecipe->starNum = $request->input('rate');
+        //         $new_raterecipe->favYesNo = $request->input('fav');
+        //         $new_raterecipe->user_id = $request->user_id;
+        //         $new_raterecipe->recipe_id = $request->recipe_id;
+        //         $res = $new_raterecipe->save();
+        //         if($res){
+        //         return back();
+        //         }
+        // }
     
     }
 }
