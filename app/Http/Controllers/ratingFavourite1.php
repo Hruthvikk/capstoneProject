a2af485647222861d6e51f4ad5ec2a8980a8da3e
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-17 20:59:45 -0500

    rating recipe unique7

--- app/Http/Controllers/ratingFavourite1.php
+++ app/Http/Controllers/ratingFavourite1.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\ratingFav;
 
 class ratingFavourite1 extends Controller
 {
     public function raterecipe(Request $request){
-        
+        $current_uid = $request->session()->get('loginUserId') ;
+        $query = "SELECT starNum FROM rating_favs where user_id=$current_uid";
+        $csn = DB::select($query);
         $new_raterecipe = new ratingFav();
-        if( ($new_raterecipe->starNum = $request->input('rate') )&& ($new_raterecipe->user_id = $request->user_id)){
+        if( ( $csn = $request->input('rate') )&& ($new_raterecipe->user_id = $request->user_id)){
             return back()->with('alreadyexists','Same rating already Exists');    
         }
         else{
@@ -20,10 +23,9 @@ class ratingFavourite1 extends Controller
                 $new_raterecipe->user_id = $request->user_id;
                 $new_raterecipe->recipe_id = $request->recipe_id;
                 $res = $new_raterecipe->save();
-        if($res){
-        return back();
-        }
-        
+                if($res){
+                return back();
+                }
         }
     
     }
