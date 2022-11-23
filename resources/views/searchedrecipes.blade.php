daf4253ed8ba74c0946f65a1a7dcbe8f08b0989a
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-23 13:13:44 -0500

    search ui

--- resources/views/searchedrecipes.blade.php
+++ resources/views/searchedrecipes.blade.php
@@ -6,9 +6,10 @@
         <div class="row">
         <h2 style="text-align: center;">Searched Recipes</h2>
             <div class="pr">
-            @foreach ($mtres as $sd )
-            <?php $imagename=$sd->recipeImage; ?>
+            
                 <div class="prr images">
+                @foreach ($mtres as $sd )
+                <?php $imagename=$sd->recipeImage; ?>
                     <a href="{{url('viewrecipe',$sd->id)}}"> <img src="/public/Image/<?=$imagename?>" alt="" height="200px" width="200px"> </a>
                     <p>{{$sd->recipeName}}</p>
                 </div>
