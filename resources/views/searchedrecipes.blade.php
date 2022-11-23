e78a104700bbf95ae4c20770b5551b8855bd4d7f
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-23 13:04:39 -0500

    admin view block by user

--- resources/views/searchedrecipes.blade.php
+++ resources/views/searchedrecipes.blade.php
@@ -3,8 +3,9 @@
 <h3>Searched Recipes</h3>
     @foreach ($mtres as $sd )
     <?php $imagename=$sd->recipeImage; ?>
-    <div class="grid-cointainer">
-        <div class="grid-item">
+    <div class="cointainer">
+        <div class="row">
+            
             <div class="pr">
             <div class="prr images">
                 <a href="{{url('viewrecipe',$sd->id)}}"> <img src="/public/Image/<?=$imagename?>" alt="" height="200px" width="200px"> </a>
