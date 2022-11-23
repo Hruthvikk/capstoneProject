ff3e4638f8f9ac15476fbfdfec5642fa49b83bf6
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-23 13:11:58 -0500

    admin view block by user

--- resources/views/searchedrecipes.blade.php
+++ resources/views/searchedrecipes.blade.php
@@ -1,15 +1,17 @@
 @extends('mainafterlogin')
 @section('content')
-<h3>Searched Recipes</h3>
-    @foreach ($mtres as $sd )
-    <?php $imagename=$sd->recipeImage; ?>
+
+    
     <div class="cointainer">
-        <div class="grid-item">
+        <div class="row">
+        <h2 style="text-align: center;">Searched Recipes</h2>
             <div class="pr">
-            <div class="prr images">
-                <a href="{{url('viewrecipe',$sd->id)}}"> <img src="/public/Image/<?=$imagename?>" alt="" height="200px" width="200px"> </a>
-                <p>{{$sd->recipeName}}</p>
-            </div>
+            @foreach ($mtres as $sd )
+            <?php $imagename=$sd->recipeImage; ?>
+                <div class="prr images">
+                    <a href="{{url('viewrecipe',$sd->id)}}"> <img src="/public/Image/<?=$imagename?>" alt="" height="200px" width="200px"> </a>
+                    <p>{{$sd->recipeName}}</p>
+                </div>
             </div>
         </div>
     </div>
