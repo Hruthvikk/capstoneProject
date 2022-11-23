85b7faa3436797daf7bd4c67a3e9db0eb6f12d90
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-23 12:59:46 -0500

    admin view block by user

--- resources/views/searchedrecipes.blade.php
+++ resources/views/searchedrecipes.blade.php
@@ -5,9 +5,11 @@
     <?php $imagename=$sd->recipeImage; ?>
     <div class="grid-cointainer">
         <div class="grid-item">
+            <div class="pr">
             <div class="prr images">
                 <a href="{{url('viewrecipe',$sd->id)}}"> <img src="/public/Image/<?=$imagename?>" alt="" height="200px" width="200px"> </a>
                 <p>{{$sd->recipeName}}</p>
+            </div>
             </div>
         </div>
     </div>
