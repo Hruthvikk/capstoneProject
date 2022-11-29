f33efcd0ef5759aab14f7327a0ba75c3ac4c9918
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-28 23:12:25 -0500

    image display try

--- resources/views/searchedrecipes.blade.php
+++ resources/views/searchedrecipes.blade.php
@@ -10,7 +10,7 @@
             @foreach ($mtres as $sd )
             <?php $imagename=$sd->recipeImage; ?>
                 <div class="prr images">
-                    <a href="{{url('viewrecipe',$sd->id)}}"> <img src="{{asset('/public/storage/'.$sd->recipeImage)}}" alt="" height="200px" width="200px"> </a>
+                    <a href="{{url('viewrecipe',$sd->id)}}"> <img src="/public/Image/<?=$imagename?>" alt="" height="200px" width="200px"> </a>
                     <p>{{$sd->recipeName}}</p>
                 </div>
                 @endforeach
