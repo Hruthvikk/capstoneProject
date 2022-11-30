8891022e2e2087b88af6e55a34dc1f2c75f2b425
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-30 05:34:20 -0500

    homeafterlogin display img2

--- app/Http/Controllers/signin.php
+++ app/Http/Controllers/signin.php
@@ -51,9 +51,9 @@ class signin extends Controller
                         $request->session()->put('loginUserId',$user->id);
                         $request->session()->put('userRole',$user->userType);
                         $rndrec = recipes::inRandomorder()->limit(3)->get();
-                        $brkfst = recipes::where('mealTime_id','=',1)->limit(1)->first();
-                        $lunch = recipes::where('mealTime_id','=',2)->limit(1)->first();
-                        $dine = recipes::where('mealTime_id','=',3)->limit(1)->first();
+                        $brkfst = recipes::select('recipes.*')->where('mealTime_id','=',1)->limit(1)->first();
+                        $lunch = recipes::select('recipes.*')->where('mealTime_id','=',2)->limit(1)->first();
+                        $dine = recipes::select('recipes.*')->where('mealTime_id','=',3)->limit(1)->first();
                         $params = [
                             'rndrec'=>$rndrec,
                             'brkfst'=>$brkfst,
