fe0677662f71ab730e5822418751db45c8cccadc
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-30 01:33:43 -0500

    testing 6 viewsteps

--- resources/views/recipesteps.blade.php
+++ resources/views/recipesteps.blade.php
@@ -1,7 +1,7 @@
 @extends('mainafterlogin')
 @section('content')
         @foreach ($recipestepdata as $rs )
-    <div>
+    <div class="displaysteps">
             <h2>Recipe Name: {{$rs->recipeName}} </h2><br>
             <h2>Steps:</h2> <br>       
             <h4 class="steps">{{$rs->steps}}</h4>
