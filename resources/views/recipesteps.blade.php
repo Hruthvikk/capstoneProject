4dba36e695bb991b5211a2508100a9f04ad5bf89
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-30 01:55:23 -0500

    testing 6.6 viewsteps

--- resources/views/recipesteps.blade.php
+++ resources/views/recipesteps.blade.php
@@ -1,13 +1,11 @@
 @extends('mainafterlogin')
 @section('content')
         @foreach ($recipestepdata as $rs )
-        <div class="gridviewrecip">
-        <div class="gridv1">
+    <div class="displaysteps">
             <h2>Recipe Name: {{$rs->recipeName}} </h2><br>
             <h2>Steps:</h2> <br>       
             <h4 class="steps">{{$rs->steps}}</h4>
-        </div>
-        </div>
+    </div>
         @endforeach
     
 @endsection
