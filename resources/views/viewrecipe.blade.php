b0054595d78a146ebe3567d53cb2066bd56d31b0
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-12-01 01:44:27 -0500

    btn css

--- resources/views/viewrecipe.blade.php
+++ resources/views/viewrecipe.blade.php
@@ -16,7 +16,7 @@
             <script language="javascript">
                 function getv() {
                     var terf = document.getElementById('terf');
-                    var terf1 = 1
+                    var terf1 = ""
                     terf1 = document.getElementById('numofperson').value;
                     var firstChilds = terf.querySelectorAll("td:first-child");
                     var allName = [];
