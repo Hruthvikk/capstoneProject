72086ec2818360264320467f55907c9f776dadc2
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-12-01 00:59:51 -0500

    btn css

--- resources/views/viewrecipe.blade.php
+++ resources/views/viewrecipe.blade.php
@@ -21,7 +21,7 @@
                 var rowCount = table.rows.length;
                 for(var i = 0; i < rowCount; i++ ){
                     table.rows[i].cell[0].innerHTML = mcol*numofppl
-                    console.log(table.rows[i].cell[0].innerHTML);
+                    console.log(table.rows.cell[0].innerHTML);
                     console.log(mcol);
                     console.log(numofppl);
                     console.log(mcol*numofppl);
