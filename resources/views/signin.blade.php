8bbe3431de0b11692de1563a5434af1cd0e25024
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-30 21:03:44 -0500

    btn css

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -22,12 +22,7 @@
                     @enderror
                 </span>
             </div>
-            <div class="form-group col-md-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="basic-addon1">@</span>
-                </div>
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-              </div>
+
             <div class="form-group col-md-3">
                 <label class="form-label">Password</label>
                 <input type="password" name="password" class="form-control" placeholder="password" />
