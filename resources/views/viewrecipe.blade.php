c39fb800949c7c956177fae8fbf09ac22177d0d6
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-12-01 01:42:49 -0500

    btn css

--- resources/views/viewrecipe.blade.php
+++ resources/views/viewrecipe.blade.php
@@ -16,7 +16,8 @@
             <script language="javascript">
                 function getv() {
                     var terf = document.getElementById('terf');
-                    var terf1 = document.getElementById('numofperson').value;
+                    var terf1 = 1
+                    terf1 = document.getElementById('numofperson').value;
                     var firstChilds = terf.querySelectorAll("td:first-child");
                     var allName = [];
                     for(i=0; i<firstChilds.length; ++i){
