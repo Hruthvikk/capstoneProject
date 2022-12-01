5bb578c38a6e7105885bb89ec0f649a06e9b57a3
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-11-30 21:54:36 -0500

    btn css

--- resources/views/viewfavourites.blade.php
+++ resources/views/viewfavourites.blade.php
@@ -1,16 +1,16 @@
 @extends('mainafterlogin')
 @section('content')
     <div class="pr">
-    @foreach ($userFavourites as $uf)
-        <div class="prr images">
-            <?php
-            $imagename = $uf->recipeImage;
-            ?>
-            <a href="{{ url('viewrecipe', $uf->id) }}"><img
-                    src="https://softwarecapstone000813765.s3.us-east-2.amazonaws.com/images/<?= $imagename ?>" alt=""
-                    height="200px" width="200px"></a>
-            <p>{{ $uf->recipeName }}</p>
-        </div>
-    @endforeach
+        @foreach ($userFavourites as $uf)
+            <div class="prr images">
+                <?php
+                $imagename = $uf->recipeImage;
+                ?>
+                <a href="{{ url('viewrecipe', $uf->id) }}"><img
+                        src="https://softwarecapstone000813765.s3.us-east-2.amazonaws.com/images/<?= $imagename ?>"
+                        alt="" height="200px" width="200px"></a>
+                <p>{{ $uf->recipeName }}</p>
+            </div>
+        @endforeach
     </div>
 @endsection
