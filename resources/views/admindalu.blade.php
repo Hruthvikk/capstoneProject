02ed273753d346e5e2a1a72c0a1cb407c5efdb67
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-12-02 18:53:02 -0500

    btn css

--- resources/views/admindalu.blade.php
+++ resources/views/admindalu.blade.php
@@ -39,7 +39,7 @@
             </thead>
             <tbody>
                 @foreach ($displayau as $au)
-                <?php console.log($au+"Hello"); ?>
+                <?php var_dimp($au+"Hello"); ?>
                     <tr>
                         <td>{{ $au->userFirstName }}</td>
                         <td>{{ $au->userLastName }}</td>
