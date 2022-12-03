ad47b5ddf65cd19d8e7b460b4960650ecee236a6
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-12-02 20:22:02 -0500

    date

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,13 +1,14 @@
 @extends('mainafterlogin')
 @section('content')
-<div class="recipeform">
-    <h2>Ask a question to chef</h2>
-    <br>
-    <h2>Question?</h2>
-    <div class="displaysteps">
-        <form>
-            @csrf
-            <textarea cols="100" name="memquestion"></textarea>
-        </form>
+    <div class="recipeform">
+        <h2>Ask a question to chef</h2>
+        <br>
+        <h2>Question?</h2>
+        <div class="displaysteps">
+            <form>
+                @csrf
+                <textarea cols="100" name="memquestion"></textarea>
+            </form>
+        </div>
     </div>
 @endsection
