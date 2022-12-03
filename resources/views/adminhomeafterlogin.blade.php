1ddd85c1bba65ec421176a9ce2bbfd2b5283ef2a
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-12-02 21:30:41 -0500

    date

--- resources/views/adminhomeafterlogin.blade.php
+++ resources/views/adminhomeafterlogin.blade.php
@@ -4,5 +4,13 @@
         <a href="{{ url('admindalu') }}" class="btn btn-primary">Display all Users Added</a>
         <br><br>
         <a href="{{ url('admindar') }}" class="btn btn-primary">Display all Recipes Added</a>
+        <br><br>
+        <a href="">
+        <select id="timefil" name="timefil">
+            <option value="today">Today</option>
+            <option value="week">Week</option>
+            <option value="month">Month</option>
+        </select>
+        </a>
     </div>
 @endsection
