b89645e3d44b4382d84748ac78273f344f2231b8
Hruthvikk <101742067+Hruthvikk@users.noreply.github.com>
2022-12-02 21:12:41 -0500

    date

--- resources/views/adminhomeafterlogin.blade.php
+++ resources/views/adminhomeafterlogin.blade.php
@@ -1,8 +1,8 @@
 @extends('adminmain')
 @section('content')
-    <h2>
+    <div style="width: 300px; margin: 0 auto;">
         <a href="{{ url('admindalu') }}" class="btn btn-primary">Display all Users Added</a>
         <br><br>
         <a href="{{ url('admindar') }}" class="btn btn-primary">Display all Recipes Added</a>
-    </h2>
+    </div>
 @endsection
